3234466e7e034d123d38027557dc8ff2afd8c743
Ben Gorman <bencodes.au@gmail.com>
2025-09-10 13:02:37 +1000

    feat: chapter 10-16 (all)

---
+++ auth.ts
@@ -0,0 +1,62 @@
+// /auth.ts
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
+import { z } from 'zod';
+import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
+import postgres from 'postgres';
+
+// Connect to your database
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+// Query a user by email
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+    return user[0];
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        // Validate incoming credentials with zod
+        const parsedCredentials = z
+          .object({
+            email: z.string().email(),
+            password: z.string().min(6),
+          })
+          .safeParse(credentials);
+
+        if (!parsedCredentials.success) {
+          console.log('Invalid credentials format');
+          return null;
+        }
+
+        const { email, password } = parsedCredentials.data;
+
+        // Look up the user
+        const user = await getUser(email);
+        if (!user) {
+          console.log('User not found');
+          return null;
+        }
+
+        // Compare passwords
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+        if (passwordsMatch) {
+          return user;
+        }
+
+        console.log('Invalid password');
+        return null;
+      },
+    }),
+  ],
+});
